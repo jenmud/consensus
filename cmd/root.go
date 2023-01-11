5729ae90419a1b0f279335a52bdbdaaaeb5e0753
Jenda Mudron <jenmud@gmail.com>
2023-01-11 21:49:40 +1100

    added in the code to serve the GraphQL server

--- cmd/root.go
+++ cmd/root.go
@@ -17,15 +17,25 @@ package cmd
 
 import (
 	"context"
-	"errors"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
 	"github.com/jenmud/consensus/ent"
+	"github.com/jenmud/consensus/graph"
+	"github.com/jenmud/consensus/graph/generated"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
 )
 
+// sqliteClient returns a sqlite3 ent client.
 func sqliteClient(dsn string) (*ent.Client, error) {
 	client, err := ent.Open("sqlite3", dsn)
 	if err != nil {
@@ -36,9 +46,79 @@ func sqliteClient(dsn string) (*ent.Client, error) {
 	return client, client.Schema.Create(context.Background())
 }
 
+// Defining the Graphql handler
+func graphqlHandler(client *ent.Client) gin.HandlerFunc {
+	h := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{Resolvers: graph.New(client)},
+		),
+	)
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// playgroundHandleer returns a GraphQL handler.
+func playgroundHandler() gin.HandlerFunc {
+	h := playground.Handler("Consensus GraphQL Playground", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// servePlayground serves the GraphQL playground.
+func servePlayground(ctx context.Context, addr string, client *ent.Client, debug bool) error {
+	go func() {
+		if !debug {
+			gin.SetMode(gin.ReleaseMode)
+		}
+
+		r := gin.Default()
+		r.POST("/query", graphqlHandler(client))
+		r.GET("/", playgroundHandler())
+
+		s := &http.Server{
+			Addr:           addr,
+			Handler:        r,
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+
+		log.Printf("listening and accepting client connections on %s", addr)
+		s.ListenAndServe()
+	}()
+
+	<-ctx.Done()
+	return nil
+}
+
 // entrypoint is the main entrypoint to the applications.
 func entrypoint(cmd *cobra.Command, args []string) error {
-	return errors.New("not implemented")
+	var client *ent.Client
+	var err error
+
+	debugging, err := strconv.ParseBool(cmd.Flags().Lookup("debug").Value.String())
+	if err != nil {
+		return err
+	}
+
+	dialect := cmd.Flags().Lookup("dialect").Value.String()
+	dsn := cmd.Flags().Lookup("db").Value.String()
+
+	switch strings.ToLower(dialect) {
+	case "sqlite3":
+		client, err = sqliteClient(dsn)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	addr := cmd.Flags().Lookup("server").Value.String()
+	return servePlayground(cmd.Context(), addr, client, debugging)
 }
 
 // rootCmd represents the base command when called without any subcommands
@@ -63,8 +143,9 @@ func Execute() {
 }
 
 func init() {
+	rootCmd.Flags().StringP("server", "s", "0.0.0.0:8080", "address to listen and accept client connections")
 	rootCmd.Flags().String("db", "file:consensus.sqlite?mode=memory&cache=shared&_fk=1", "DSN string for connecting to the database")
-	rootCmd.Flags().String("dialect", "sqlite3", "Dialect used for the sql driver")
+	rootCmd.Flags().String("dialect", "sqlite3", "dialect used for the sql driver")
 	rootCmd.Flags().Bool("debug", false, "enable sql debugging")
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("toggle", "t", false, "help message for toggle")
 }
--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,16 @@
 package graph
 
+import "github.com/jenmud/consensus/ent"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	client *ent.Client
+}
+
+// New returns a new graph resolver.
+func New(client *ent.Client) *Resolver {
+	return &Resolver{client: client}
+}
