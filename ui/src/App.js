a455d58a6906a38d856f0c9125e6ec91d6b0b028
Jenda Mudron <jenmud@gmail.com>
2023-01-12 21:34:38 +1100

    some starting points for the ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,13 @@
+import { Typography } from "@mui/material";
+
 function App() {
-  return <div className="App">Hi world</div>;
+  return (
+    <div className="App">
+      <Typography variant="h1" component="p">
+        Hi world
+      </Typography>
+    </div>
+  );
 }
 
 export default App;
