2938d392b82cda2fca576a3e835fc99da54fabb0
Jenda Mudron <jenmud@gmail.com>
2023-02-25 16:23:31 +1100

    added in the headers

--- ui/src/components/draggable.jsx
+++ ui/src/components/draggable.jsx
@@ -1,6 +1,6 @@
 
 import React, {useState} from 'react';
-import { Box, Paper, Stack } from "@mui/material";
+import { Box, Paper, Stack, Typography } from "@mui/material";
 import {DndContext} from '@dnd-kit/core';
 import {useDroppable} from '@dnd-kit/core';
 import {useDraggable} from '@dnd-kit/core';
@@ -41,12 +41,15 @@ export default function Swimlane() {
   return (
     <Stack direction="row" spacing={2} justifyContent="space-between" alignItems="center">
       <Box sx={{ width: "100%", height: "90vh", backgroundColor: '#E5E8E8', '&:hover': { backgroundColor: '#C9E7E7', opacity: [0.9, 0.8, 0.7] }}}>
+        <Typography variant="h4" gutterBottom sx={{textAlign: 'center'}}>To Do</Typography>
       </Box>
 
       <Box sx={{ width: "100%", height: "90vh", backgroundColor: '#C1F5E9', '&:hover': { backgroundColor: '#3AF4C9', opacity: [0.9, 0.8, 0.7] }}}>
+        <Typography variant="h4" gutterBottom sx={{textAlign: 'center'}}>In Progress</Typography>
       </Box>
 
       <Box sx={{ width: "100%", height: "90vh", backgroundColor: '#CBE1F7', '&:hover': { backgroundColor: '#3897F5', opacity: [0.9, 0.8, 0.7] }}}>
+        <Typography variant="h4" gutterBottom sx={{textAlign: 'center'}}>Done</Typography>
       </Box>
     </Stack>
   );
