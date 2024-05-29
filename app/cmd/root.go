11bf78cf6219ec60fef07ca279e5d2a4ade3acd5
Jenda Mudron <jenmud@gmail.com>
2024-05-29 22:05:30 +1000

    typo fix

--- app/cmd/root.go
+++ app/cmd/root.go
@@ -61,7 +61,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.app.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.concensus.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
@@ -81,7 +81,7 @@ func initConfig() {
 		// Search config in home directory with name ".app" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".app")
+		viper.SetConfigName(".concencus")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
